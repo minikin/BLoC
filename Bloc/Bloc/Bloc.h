6c6e2d0f40485884bdb707ddf895b90d681e5cbb
Sasha Prokhorenko <djminikin@gmail.com>
2019-04-22 20:21:30 +0200

    Update project structure

---
+++ Bloc/Bloc/Bloc.h
@@ -0,0 +1,19 @@
+//
+//  Bloc.h
+//  Bloc
+//
+//  Created by Sasha Prokhorenko on 22.04.19.
+//  Copyright © 2019 Sasha Prokhorenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bloc.
+FOUNDATION_EXPORT double BlocVersionNumber;
+
+//! Project version string for Bloc.
+FOUNDATION_EXPORT const unsigned char BlocVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bloc/PublicHeader.h>
+
+
--- Example/Tests/Tests.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Tests.swift
-//  Bloc_Example
-//
-//  Created by Sasha Prokhorenko on 22.04.19.
-//  Copyright © 2019 CocoaPods. All rights reserved.
-//
-
-import Foundation
