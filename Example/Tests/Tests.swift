68d72e0f06ad2afc1a2e08a867fcf4fab299bc93
Sasha Prokhorenko <djminikin@gmail.com>
2019-04-22 19:36:19 +0200

    Updtae pods

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,50 +1,9 @@
-// https://github.com/Quick/Quick
+//
+//  Tests.swift
+//  Bloc_Example
+//
+//  Created by Sasha Prokhorenko on 22.04.19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
 
-import Quick
-import Nimble
-import Bloc
-
-class TableOfContentsSpec: QuickSpec {
-    override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
-            
-            context("these will pass") {
-
-                it("can do maths") {
-                    expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    DispatchQueue.main.async {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
-                }
-            }
-        }
-    }
-}
+import Foundation
