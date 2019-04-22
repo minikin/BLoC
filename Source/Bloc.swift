a63f19e1fd0c558e8aca2dc245cff8a63a477012
Sasha Prokhorenko <djminikin@gmail.com>
2019-04-22 20:11:38 +0200

    Fix warnings

--- Source/Bloc.swift
+++ Source/Bloc.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+import RxSwift
+
+/// Takes a [Stream] of [Event]s as input
+/// and transforms them into a [Stream] of [State]s as output.
+struct Bloc<Event, State> {
+    let eventSubject = PublishSubject<Event>();
+    
+}
